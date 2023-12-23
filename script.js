1b15ccd68a3d13108ce046a993d89e64ba976d0b
Hoàng Thế <2251012061hoang@ou.edu.vn>
2023-12-23 23:37:22 +0700

    Update styles and add registration page

--- script.js
+++ script.js
@@ -32,8 +32,8 @@ registerLink.addEventListener('click', (event) => {
 var dropdownContent = document.createElement('div');
 dropdownContent.className = 'dropdown-content';
 
-var brands = ["Logitech", "Corsair", "DareU", "Razer", "Steelseries", "AKKO", "All the products"];
-var links = ["https://www.logitechg.com/vi-vn", "https://www.corsair.com/us/en/", "https://dareu.com.vn", "https://www.razer.com", "https://steelseries.com/", "https://en.akkogear.com", "keyboard-main.html"];
+var brands = ["Logitech", "Corsair", "DareU", "Razer", "Steelseries", "AKKO"];
+var links = ["https://www.logitechg.com/vi-vn", "https://www.corsair.com/us/en/", "https://dareu.com.vn", "https://www.razer.com", "https://steelseries.com/", "https://en.akkogear.com"];
 
 for (var i = 0; i < brands.length; i++) {
     var link = document.createElement('a');
@@ -46,66 +46,6 @@ var dropdown = document.querySelector('.dropdown');
 dropdown.appendChild(dropdownContent);
 
 //---------------------------------------------------------------------------------------------
-
-// JavaScript code to auto-generate product cards
-// window.onload = function() {
-//     var products = [
-//         {name: "Corsair K70 Pro", price: 3490000, img: "img/Keyboard/Crs_kb2.png"},
-//         {name: "Corsair K55 RGB Pro Lite", price: 880000, img: "img/Keyboard/Crs_kb1.png"},
-//         {name: "Corsair K70 TKL RGB Champion Series", price: 3500000, img: "img/Keyboard/Crs_kb3.png"},
-//         {name: "AKKO 5075B Plus", price: 2400000, img: "img/Keyboard/ako_kb1.png"},
-//         {name: "AKKO 3108 v2 DS Horizon", price: 2400000, img: "img/Keyboard/ako_kb2.png"},
-//         {name: "AKKO 5108S Black Pink", price: 2400000, img: "img/Keyboard/ako_kb3.png"},
-//         {name: "Steelseries Apex PRO TKL", price: 4150000, img: "img/Keyboard/Ssr_kb1.png"},
-//         {name: "Steelseries Apex 7 TKL", price: 3500000, img: "img/Keyboard/Ssr_kb2.png"},
-//         {name: "Steelseries Apex 3", price: 2000000, img: "img/Keyboard/Ssr_kb3.png"},
-//         {name: "Logitech G915 TKL", price: 4500000, img: "img/Keyboard/Lgt_kb1.png"},
-//         {name: "Logitech G512 Carbon", price: 2500000, img: "img/Keyboard/Lgt_kb2.png"},
-//         {name: "Logitech G213 Prodigy", price: 1500000, img: "img/Keyboard/Lgt_kb3.png"},
-//         {name: "Razer BlackWidow V3 Pro", price: 5000000, img: "img/Keyboard/Rzr_kb1.png"},
-//         {name: "Razer BlackWidow V3 Tenkeyless", price: 3500000, img: "img/Keyboard/Rzr_kb2.png"},
-//         {name: "Razer BlackWidow V3", price: 3000000, img: "img/Keyboard/Rzr_kb3.png"},
-//         {name: "DareU EK820", price: 500000, img: "img/Keyboard/Dru_kb1.png"},
-//         {name: "DareU EK75 Pro", price: 600000, img: "img/Keyboard/Dru_kb2.png"},
-//         {name: "DareU EK815", price: 400000, img: "img/Keyboard/Dru_kb3.png"}
-//     ];
-
-//     var container = document.querySelector('.row1'); 
-
-//     for (var i = 0; i < 6; i++) {
-//         var column = document.createElement('div');
-//         column.className = 'column';
-
-//         var card = document.createElement('div');
-//         card.className = 'card';
-
-//         var img = document.createElement('img');
-//         img.src = products[i].img;
-
-//         var description = document.createElement('div');
-//         description.className = 'description';
-
-//         var prod_Name = document.createElement('div');
-//         prod_Name.className = 'prod_Name';
-//         prod_Name.innerHTML = '<strong>' + products[i].name + '<strong>';
-
-//         var prod_Price = document.createElement('div');
-//         prod_Price.className = 'prod_Price';
-//         prod_Price.innerText = parseInt(products[i].price).toLocaleString('de-DE') + "₫";
-
-//         var addToCart = document.createElement('button');
-//         addToCart.innerText = 'Add to Cart';
-//         addToCart.className = 'addToCart';
-
-//         description.appendChild(prod_Name);
-//         description.appendChild(prod_Price);
-//         description.appendChild(addToCart);
-//         card.appendChild(img);
-//         card.appendChild(description);
-//         column.appendChild(card);
-//         container.appendChild(column);
-//     }
-// }
 
 window.onload = function() {
     var products = [
@@ -195,3 +135,11 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 //---------------------------------------------------------------------------------------------
 
+document.querySelector('.search_ic').addEventListener('click', function() {
+    var searchBar = document.getElementById('searchBar');
+    if (searchBar.style.display === 'none') {
+      searchBar.style.display = 'block';
+    } else {
+      searchBar.style.display = 'none';
+    }
+  });
